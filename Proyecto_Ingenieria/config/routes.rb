a62e21b46144ded586d55722dfdff9ec8a4c34b6
Maria Apolo <maria.apolo@sansano.usm.cl>
2018-08-03 02:34:33 -0400

    registro/lista materiales

--- Proyecto_Ingenieria/config/routes.rb
+++ Proyecto_Ingenieria/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/new_material', to:'materials#new'
+  resources :materials
+
+  get '/list_materials', to:'materials#list_materials'
+  resources :materials
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
